a07329d114180be39ee259dcf459e89f0d303ee1
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2018-04-18 12:04:31 +0200

    release 1.5

--- server/project.clj
+++ server/project.clj
@@ -1,7 +1,7 @@
-(def +version+ "1.4-SNAPSHOT")
+(def +version+ "1.6-SNAPSHOT")
 (def +slipstream-version+ "3.48")
 
-(defproject eu.mf2c-project.cimi/server "1.4-SNAPSHOT"
+(defproject eu.mf2c-project.cimi/server "1.6-SNAPSHOT"
 
   :description "MF2C CIMI resources and server"
 
