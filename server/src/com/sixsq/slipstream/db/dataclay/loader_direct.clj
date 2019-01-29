ad4cdfc30f787e4cb6a264fe4b87649a4c97843a
Charles Loomis <cal@sixsq.com>
2019-01-29 10:09:02 +0100

    add logging for send-fn

--- server/src/com/sixsq/slipstream/db/dataclay/loader_direct.clj
+++ server/src/com/sixsq/slipstream/db/dataclay/loader_direct.clj
@@ -2,11 +2,19 @@
   (:refer-clojure :exclude [load])
   (:require
     [com.sixsq.dataclay.handler :as proxy]
-    [com.sixsq.slipstream.db.dataclay.binding :as dataclay]))
+    [com.sixsq.slipstream.db.dataclay.binding :as dataclay]
+    [clojure.tools.logging :as log]))
+
+
+(defn send-fn
+  [argv]
+  (log/info "dataClay direct binding argv:" argv)
+  (let [response (proxy/scrud-action argv)]
+    (log/info "dataClay direct binding scrud response:" response)
+    (second response)))
 
 
 (defn load
   "Creates a dataClay database binding that directly accesses the database."
   []
-  (let [send-fn #(-> % proxy/scrud-action second)]
-    (dataclay/->DataClayBinding send-fn)))
+  (dataclay/->DataClayBinding send-fn))
