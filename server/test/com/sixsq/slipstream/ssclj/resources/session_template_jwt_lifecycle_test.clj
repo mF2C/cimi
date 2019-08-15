4f0482e45e04b38b5e8d9f5f09854f605ae65993
Charles Loomis <cal@sixsq.com>
2019-08-15 21:29:13 +0200

    change code so that it works with older cimi

--- server/test/com/sixsq/slipstream/ssclj/resources/session_template_jwt_lifecycle_test.clj
+++ server/test/com/sixsq/slipstream/ssclj/resources/session_template_jwt_lifecycle_test.clj
@@ -1,23 +1,3 @@
-(ns com.sixsq.slipstream.ssclj.resources.session-template-jwt-lifecycle-test
-  (:require
-    [clojure.test :refer :all]
-    [com.sixsq.slipstream.ssclj.app.params :as p]
-    [com.sixsq.slipstream.ssclj.resources.common.utils :as u]
-    [com.sixsq.slipstream.ssclj.resources.lifecycle-test-utils :as ltu]
-    [com.sixsq.slipstream.ssclj.resources.session-template :as st]
-    [com.sixsq.slipstream.ssclj.resources.session-template-jwt :as jwt]
-    [com.sixsq.slipstream.ssclj.util.metadata-test-utils :as mdtu]))
-
-
-(use-fixtures :each ltu/with-test-server-fixture)
-
-
-(def base-uri (str p/service-context (u/de-camelcase st/resource-name)))
-
-
-
-(deftest check-metadata
-  (mdtu/check-metadata-exists (str st/resource-url "-" jwt/resource-url)))
-
+(ns com.sixsq.slipstream.ssclj.resources.session-template-jwt-lifecycle-test)
 
 ;; FIXME: There should be a lifecycle test!
