8a2c99beab185543692b0e7cb25a36b470cb316d
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-10-02 11:35:44 +0200

    add mocking hook to lm

--- server/src/com/sixsq/slipstream/ssclj/resources/service_instance.clj
+++ server/src/com/sixsq/slipstream/ssclj/resources/service_instance.clj
@@ -6,13 +6,22 @@
     [com.sixsq.slipstream.ssclj.resources.common.schema :as c]
     [com.sixsq.slipstream.ssclj.resources.common.std-crud :as std-crud]
     [com.sixsq.slipstream.ssclj.resources.common.utils :as u]
+    [clojure.data.json :as json]
+    [clj-http.client :as http]
     [com.sixsq.slipstream.ssclj.resources.spec.service-instance]
+    [clojure.string :as stringer]
     [superstring.core :as str]))
 
 (def ^:const resource-name "ServiceInstance")
 (def ^:const resource-tag (keyword (str (str/camel-case resource-name) "s")))
 (def ^:const resource-url (u/de-camelcase resource-name))
 (def ^:const collection-name "ServiceInstanceCollection")
+
+(defn ns->type
+  [ns]
+  (-> ns str (stringer/split #"\.") last))
+
+(def ^:const resource-type (ns->type *ns*))
 
 (def ^:const resource-uri (str c/cimi-schema-uri resource-name))
 (def ^:const collection-uri (str c/cimi-schema-uri collection-name))
@@ -29,6 +38,26 @@
 (defmethod crud/add-acl resource-uri
   [resource request]
   (a/add-acl resource request))
+
+
+
+
+(defn call-sm
+  "Returns a tuple with status and message"
+  [url body]
+  ; (if (and url body)
+  ;   (try
+  (:service-instance (json/read-str (:body (http/post url
+                                             {:headers      {"Accept" "application/json"}
+                                              :content-type :json
+                                              :accept       :json
+                                              :body         (json/write-str body)})) :key-fn keyword)
+    )
+  ; (catch Exception e
+  ;   (ex-data e))  )
+  ; [412 "Incomplete"]))
+  )
+
 
 ;;
 ;; "Implementations" of multimethod declared in crud namespace
@@ -79,3 +108,40 @@
 (defn initialize
   []
   (std-crud/initialize resource-url :cimi/service-instance))
+
+
+;;
+;;
+;; Custom operation
+;;
+
+;;
+;; Redeem operation
+;;
+
+
+(defn operation-map
+  "Provides the operation map for the given href and operation."
+  [href op-kw-or-name]
+  {:rel  (name op-kw-or-name)
+   :href href})
+
+
+(defn action-map
+  "Provides the operation map for an action, which always has a relative path
+   to the resource's id."
+  [id op-kw-or-name]
+  (let [href (str id "/" (name op-kw-or-name))]
+    (operation-map href op-kw-or-name)))
+
+(defmethod crud/do-action [resource-type "deploy"]
+  [{{uuid :uuid} :params :as request}]
+  (add-impl (assoc request :body (call-sm "http://lm-um:46000/api/v2/lm/service-instance-int" (:body request)))))
+
+
+(defmethod crud/set-operations resource-type
+  [{:keys [id state] :as resource} request]
+  (let [deploy-op (action-map id :deploy)]
+    (cond-> (crud/set-standard-operations resource request)
+            (and true?) (update :operations conj deploy-op)
+           )))
